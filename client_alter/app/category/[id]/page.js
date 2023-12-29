030fbec19932d23d00bf677a3378967ed25d98c0
dlwjdgns0308 <dlwjdgns0308@naver.com>
2023-12-29 14:02:05 +0900

    feature: 카테고리 페이지 추가

---
+++ client_alter/app/category/[id]/page.js
@@ -0,0 +1,10 @@
+
+export default async function ItemPage(props) {
+  const category = props.params.id;
+    return (
+      <div>
+        <h1>the {category} Page</h1>
+      </div>
+    );
+  }
+  
