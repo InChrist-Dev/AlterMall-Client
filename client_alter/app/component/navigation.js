333c01eb8603a5e8e25fb1af595216ac9864f033
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-01-03 14:24:15 +0900

    refactor: 사장님 소개 페이지 디자인 수정

--- client_alter/app/component/navigation.js
+++ client_alter/app/component/navigation.js
@@ -1,4 +1,4 @@
-// NavigationBar.jsx
+'use client'
 import { faSearch, faShoppingCart, faUser, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
--- client_alter/app/seller/page.js
+++ client_alter/app/seller/page.js
@@ -1,42 +1,58 @@
 // seller.js
-
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import styles from './seller.module.css';
 
-const EpisodeItem = ({ episode }) => (
-  <div className={styles.episodeItem}>
-    {/* 에피소드 아이템 내용 */}
-    <p>{episode.title}</p>
-    {/* 에피소드 정보 등 */}
-  </div>
-);
-
-const Carousel = ({ episodes }) => (
-  <div className={styles.carousel}>
-    {episodes.map((episode, index) => (
-      <EpisodeItem key={index} episode={episode} />
-    ))}
-  </div>
-);
-
 const DramaDetailPage = () => {
-  // 가상의 에피소드 데이터
-  const episodes = [
-    { title: '에피소드 1', description: '에피소드 1 설명' },
-    { title: '에피소드 2', description: '에피소드 2 설명' },
-    // 추가 에피소드 데이터
-  ];
+  const [episodes, setEpisodes] = useState([
+    '햄버거',
+    '치킨',
+    '낙곱새','피자',
+    '햄버거',
+    '치킨',
+    '낙곱새','피자',
+    
+  ]);
+
+  const [episodesImage, setEpisodesImage] = useState([
+    '/food/ham.jpg','/food/chicken.jpg','/food/nack.jpg', '/food/pizza.jpg',  '/food/ham.jpg','/food/chicken.jpg','/food/nack.jpg', '/food/pizza.jpg', 
+  ]);
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const transformValue = `translateX(${-currentSlide * 420}px)`; // 에피소드 카드의 너비(200px) + 간격(20px)을 기준으로 계산
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === episodes.length - 1 ? 0 : prevSlide + 1));
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === 0 ? episodes.length - 1 : prevSlide - 1));
+  };
 
   return (
     <div className={styles.container}>
-      <div className={styles.backgroundOverlay }>
-        <img className={styles.backgroundImage} src='squid.jpg'/>
-      </div>
+      <div className={styles.background}></div>
       <div className={styles.content}>
-        <h1 className={styles.title}>사장님가게로고: 무슨사장님</h1>
+        <h1 className={styles.title}>타고난 고기 전문가: 백종찬 사장님</h1>
         <button className={styles.subscribeButton}>구독</button>
-        <p className={styles.description}>가게 설명 Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-        <Carousel episodes={episodes} />
+        <p className={styles.description}>
+        우리 고깃집은 육류 프리 고기만 취급합니다.우리 고깃집은 육류 프리 고기만 취급합니다.우리 고깃집은 육류 프리 고기만 취급합니다.우리 고깃집은 육류 프리 고기만 취급합니다.우리 고깃집은 육류 프리 고기만 취급합니다.우리 고깃집은 육류 프리 고기만 취급합니다.우리 고깃집은 육류 프리 고기만 취급합니다.
+        </p>
+        <div className={styles.episodeContainer}>
+          <h2 className={styles.episodeTitle}>음식 목록</h2>
+          <div className={styles.episodeSlider} style={{ transform: transformValue }}>
+            {episodes.map((episode, i) => (
+              <div key={i} className={styles.episodeCard}>
+                <img className={styles.episodeImage} src={episodesImage[i]}/>
+              </div>
+            ))}
+          </div>
+          <button className={styles.arrowButton} onClick={prevSlide}>
+            이전
+          </button>
+          <button className={styles.arrowButton} onClick={nextSlide}>
+            다음 
+          </button>
+        </div>
       </div>
     </div>
   );
