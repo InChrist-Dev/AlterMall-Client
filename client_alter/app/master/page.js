d1840df39f2af27b26c51f97dc65ff415fc1c648
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-01-10 18:21:55 +0900

    refactor: 장인소개 페이지 디자인 수정

--- client_alter/app/master/page.js
+++ client_alter/app/master/page.js
@@ -2,6 +2,8 @@
 'use client'
 import React, { useState } from 'react';
 import styles from './master.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 export default function Recommend() {
   const [masterCategory, setMasterCategory] = useState(['글루텐 장인', '슈가 장인', '락토 장인', '알러지 장인']);
 
@@ -9,33 +11,54 @@ export default function Recommend() {
   const [categoryPrice, setCategoryPrice] = useState(['맛으로 승부하겠습니다', '건강으로 승부하겠습니다', '건강한 음식, 맛있는 음식', '아이들 안성맞춤 건강식',]);
   const [categoryS, setCategoryS] = useState(['Top 음료', 'Top디저트', 'Top샐러드', 'Top 케이크']);
   const [categoryImage, SetCategoryImage] = useState(['/chef/chef1.jpg', '/chef/chef2.jpg', '/chef/chef3.jpg', '/chef/chef4.jpg']);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+// 좌우 이동 함수 추가
+const goToPrevSlide = () => {
+  const newIndex = (currentIndex - 1 + masterName.length) % masterName.length;
+  setCurrentIndex(newIndex);
+};
+
+const goToNextSlide = () => {
+  const newIndex = (currentIndex + 1) % masterName.length;
+  setCurrentIndex(newIndex);
+};
   return (
 
     <div style={{ width: '80%', margin: '0 auto', paddingTop: '50px' }}>
-      <p className={styles.categoryTitle}>장인 소개<span className={styles.categorySub}> |얼터몰의 장인들을 소개합니다!</span></p>
+   
       {/* 장인 버튼 */}
       {masterCategory.map((masterCategory, i) => {
         return (
           <div className={styles.masterContainer}>
-            {masterCategory}
+               <p className={styles.categoryTitle}>{masterCategory}<span className={styles.categorySub}> |얼터몰의 장인들을 소개합니다!</span></p>
             <div className={styles.artisanButtons}>
 
               {masterName.map((masterName, i) => {
                 return (
-                  <div className={styles.re_container}>
-                    <button className={styles.re_smallButton} style={{ backgroundImage: `url(${categoryImage[i]})` }}></button>
+                  <div className={styles.re_container} key={i} style={{ transform: `translateX(${-currentIndex * 100}%)` }}>
+  <button className={styles.re_smallButton} style={{ backgroundImage: `url(${categoryImage[i]})` }}></button>
+  <div className={styles.recommend_title}>{masterName}</div>
+  <div className={styles.recommend_image}>{categoryS[i]}</div>
+  <div className={styles.recommend_price}>{categoryPrice[i]}</div>
+</div>
 
-                    <div className={styles.recommend_title}>{masterName}</div>
-                    <div className={styles.recommend_image}>{categoryS[i]}</div>
-                    <div className={styles.recommend_price}>{categoryPrice[i]}</div>
-
-                  </div>
                 )
 
               })}
 
 
             </div>
+            <FontAwesomeIcon
+        icon={faChevronLeft}
+        className={styles.leftArrow}
+        onClick={goToPrevSlide}
+      />
+      <FontAwesomeIcon
+        icon={faChevronRight}
+        className={styles.rightArrow}
+        onClick={goToNextSlide}
+      />
           </div>)
       })}
     </div>);
