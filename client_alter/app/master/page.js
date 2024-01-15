c3f274515ba7784bd809fdb16c3398b80db2bb0c
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-01-15 14:52:17 +0900

    refactor:장인추천 링크연결

--- client_alter/app/master/page.js
+++ client_alter/app/master/page.js
@@ -10,7 +10,7 @@ export default function Recommend() {
   const [categoryPrice, setCategoryPrice] = useState(['맛으로 승부하겠습니다', '건강으로 승부하겠습니다', '건강한 음식, 맛있는 음식', '아이들 안성맞춤 건강식',]);
   const [categoryS, setCategoryS] = useState(['Top 음료', 'Top디저트', 'Top샐러드', 'Top 케이크']);
   const [categoryImage, SetCategoryImage] = useState(['/chef/chef1.jpg', '/chef/chef2.jpg', '/chef/chef3.jpg', '/chef/chef4.jpg']);
-  const [currentIndex, setCurrentIndex] = useState(0);
+const [currentIndex, setCurrentIndex] = useState(0);
 
 // 좌우 이동 함수 추가
 const goToPrevSlide = () => {
@@ -25,23 +25,25 @@ const goToNextSlide = () => {
   return (
 
     <div style={{ width: '80%', margin: '0 auto', paddingTop: '50px' }}>
-   
+      
       {/* 장인 버튼 */}
       {masterCategory.map((masterCategory, i) => {
         return (
           <div className={styles.masterContainer}>
-               <p className={styles.categoryTitle}>{masterCategory}<span className={styles.categorySub}> |얼터몰의 장인들을 소개합니다!</span></p>
+            <p className={styles.categoryTitle}>{masterCategory}<span className={styles.categorySub}> |얼터몰의 장인들을 소개합니다!</span></p>
             <div className={styles.artisanButtons}>
 
               {masterName.map((masterName, i) => {
                 return (<a href='/seller'>
-                  <div className={styles.re_container} key={i} style={{ transform: `translateX(${-currentIndex * 100}%)` }}>
+                  <div className={styles.re_container} key={i} style={{ transform: `translateX(${-currentIndex * 110}%)` }}>
                     <button className={styles.re_smallButton} style={{ backgroundImage: `url(${categoryImage[i]})` }}></button>
+
                     <div className={styles.recommend_title}>{masterName}</div>
                     <div className={styles.recommend_image}>{categoryS[i]}</div>
                     <div className={styles.recommend_price}>{categoryPrice[i]}</div>
+
                   </div>
-                </a>
+</a>
 
                 )
 
@@ -49,7 +51,7 @@ const goToNextSlide = () => {
 
 
             </div>
-            <FontAwesomeIcon
+<FontAwesomeIcon
         icon={faChevronLeft}
         className={styles.leftArrow}
         onClick={goToPrevSlide}
