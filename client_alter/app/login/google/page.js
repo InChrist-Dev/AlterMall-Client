e45ed51ba3518c7d5831c4a623d8fd4555b5c042
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-01 21:16:47 +0900

    f

--- client_alter/app/login/google/page.js
+++ client_alter/app/login/google/page.js
@@ -1,17 +1,33 @@
-'use client';
-import { faSearch, faShoppingCart, faUser, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { signOut } from 'next-auth/react'
 
-const LogOutBtn=(req,res)=>{
+import { useSearchParams } from "next/navigation";
+
+
+export default async function handler(req,res){
   try{
-    const query = req.query;
-    return ( <div>{query}</div>)
+    const router = useSearchParams();
+    const query = router;
+    console.log(`${query} query is`)
+
+    const url = `https://udtown.site/auth/google/login/redirect`;
+    const fetchData = async () => {
+      await fetch(url, {
+      method:'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      redirect:'follow',
+      body: 'router',
+
+
+
+    })};
+    fetchData();
+    return (<div>success</div>);
   }
   catch(error){
     console.error(error);
+    return (<div>fail</div>);
   }
  
   } 
 
-export default LogOutBtn;
