022c1c6455b6b329e862fe6713b0f761420ee75d
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-02 15:27:28 +0900

    fix:주소수정

--- client_alter/app/login/google/page.js
+++ client_alter/app/login/google/page.js
@@ -6,23 +6,23 @@ import { useSearchParams } from "next/navigation";
 
 export default async function handler(){
   try{
-  //   const params = useSearchParams();
-  //   const query = params.get('code');
-  //   console.log(`${query} query is`)
+    const params = useSearchParams();
+    const query = params.get('code');
+    console.log(`${query} query is`)
 
-  //   const url = `https://udtown.site/auth/google/login/redirect?code=${query}`;
-  //   const fetchData = async () => {
-  //     const response = await fetch(url, {
-  //     method:'get',
+    const url = `https://udtown.site/auth/google/login/`;
+    const fetchData = async () => {
+      const response = await fetch(url, {
+      method:'get',
    
-  //     redirect:'follow',
+      redirect:'follow',
      
 
-  //   });
-  //   const data = await response.status;
-  //   console.log(data);
-  // };
-  //   fetchData();
+    });
+    const data = await response.status;
+    console.log(data);
+  };
+    fetchData();
     console.log('dd');
     return (<div>success</div>);
   }
