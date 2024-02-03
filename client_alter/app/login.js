f14a8b57bdff3e6e1f248aed58feb2bb7a3e2144
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-03 09:55:12 +0900

    fix: 로그인 주소수정

--- client_alter/app/login.js
+++ client_alter/app/login.js
@@ -14,12 +14,14 @@ export function LoginBtn() {
     // const query = params.get('code');
     // console.log(`${query} query is`)
 
-    const url = 'https://udtown.site/auth/login';
+    const url = 'https://udtown.site/auth/google';
     const fetchData = async () => {
       const response = await fetch(url, {
 
     });
+    const data2 = await response.status;
     const data = await response.json();
+    console.log(data2);
     console.log(data);
     console.log('dd');
   };
