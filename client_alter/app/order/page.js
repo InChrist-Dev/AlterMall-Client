d852fb9b605bd8fc0777744e3ee87c19f7e56182
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-05 21:01:02 +0900

    d

--- client_alter/app/order/page.js
+++ client_alter/app/order/page.js
@@ -165,9 +165,9 @@ const Checkout = () => {
         배송지 변경 ▶
       </button></div>
             <div className={styles.AddressBox}>
-            {deliveryList.name}
-            {/* <div>{deliveryList[0].addr} {deliveryList[0].addr_detail}</div>
-            <div>{deliveryList[0].phone}</div> */}
+            {deliveryList? deliveryList[0].name:''}
+            <div>{deliveryList? deliveryList[0].addr:''} {deliveryList?deliveryList[0].addr_detail:''}</div>
+            <div>{deliveryList[0].phone}</div>
             </div>
             <div style={{border:'1px solid #ccc',marginTop:'50px',marginBottom:'20px'}}></div>
             <div className={styles.postBox}>
