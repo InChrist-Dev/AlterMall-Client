98fb09e19234a823697d4ac95b5cef896cfe2a0b
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-06 15:02:40 +0900

    fadsggda

--- client_alter/pages/api/payments.js
+++ client_alter/pages/api/payments.js
@@ -9,40 +9,40 @@ export default async function handler(req, res) {
     const secretKey = 'test_sk_24xLea5zVAoPKMyLlpbm8QAMYNwW';
     const url = `https://api.tosspayments.com/v1/payments/confirm`;
     const basicToken = Buffer.from(`${secretKey}:`, `utf-8`).toString("base64");
-    await fetch(url,{
-        method:'post',
-        body:JSON.stringify({
-           orderId,
-            paymentKey, 
-             amount,
+    // await fetch(url,{
+    //     method:'post',
+    //     body:JSON.stringify({
+    //        orderId,
+    //         paymentKey, 
+    //          amount,
 
-        }),
-        headers:{
-            Authorization:`Basic ${basicToken}`,
-            "Content-Type":"application/json",
-        },
-    }).then((res)=> res.json());
+    //     }),
+    //     headers:{
+    //         Authorization:`Basic ${basicToken}`,
+    //         "Content-Type":"application/json",
+    //     },
+    // }).then((res)=> res.json());
    
-    // try {
-    //     await fetch('https://udtown.site/customer/confirm/', {
-    //         method: 'patch',
-    //         body: JSON.stringify({
-    //             "order_id": orderId,
-    //             "payment_key": paymentKey,
-    //             "amount": amount,
+    try {
+        await fetch('https://udtown.site/customer/confirm/', {
+            method: 'patch',
+            body: JSON.stringify({
+                "order_id": orderId,
+                "payment_key": paymentKey,
+                "amount": amount,
 
 
-    //         }),
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //             Authorization: `Bearer ${accessToken}`,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${accessToken}`,
                
-    //         },
-    //         credentials: 'include',
-    //     }).then((res) => console.log(res.json()));
-    // } catch (error) {
-    //     console.error('데이터를 불러오는 중 오류가 발생했습니다:', error);
-    // }
+            },
+            credentials: 'include',
+        }).then((res) => console.log(res.json()));
+    } catch (error) {
+        console.error('데이터를 불러오는 중 오류가 발생했습니다:', error);
+    }
 
 
 
