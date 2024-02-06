2507498e32093ff9bfbb7a1a2cc5ee915d05ac10
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-06 13:37:08 +0900

    asd

--- client_alter/pages/api/payments.js
+++ client_alter/pages/api/payments.js
@@ -6,18 +6,30 @@ const accessToken = Cookies.get('accessToken');
 export default async function handler(req, res) {
     const { orderId, paymentKey, amount } = req.query;
     console.log(req.query)
-    // const secretKey = 'test_sk_24xLea5zVAoPKMyLlpbm8QAMYNwW';
-    // const url = `https://api.tosspayments.com/v1/payments/confirm`;
-    // const basicToken = Buffer.from(`${secretKey}:`, `utf-8`).toString("base64");
+    const secretKey = 'test_sk_24xLea5zVAoPKMyLlpbm8QAMYNwW';
+    const url = `https://api.tosspayments.com/v1/payments/confirm`;
+    const basicToken = Buffer.from(`${secretKey}:`, `utf-8`).toString("base64");
+    await fetch(url,{
+        method:'post',
+        body:JSON.stringify({
+            "order_id": orderId,
+            "payment_key": paymentKey, 
+            "amount": amount,
 
+        }),
+        headers:{
+            Authorization:`Basic ${basicToken}`,
+            "Content-Type":"application/json",
+        },
+    }).then((res)=> res.json());
    
     try {
         await fetch('https://udtown.site/customer/confirm/', {
             method: 'patch',
             body: JSON.stringify({
-                "order_id": orderId,
-                "payment_key": paymentKey,
-                "amount": amount,
+                orderId,
+                paymentKey,
+                 amount,
 
 
             }),
@@ -32,24 +44,8 @@ export default async function handler(req, res) {
         console.error('데이터를 불러오는 중 오류가 발생했습니다:', error);
     }
 
-    // await fetch(url,{
-    //     method:'post',
-    //     body:JSON.stringify({
-    //         "order_id": orderId,
-    //         "payment_key": paymentKey,
-    //         "addr": "rich building",
-    //         "addr_detail":"5th floor",
-    //         "name":name,
-    //         "amount": amount,
-    //         "phone": '2121921'
 
-    //     }),
-    //     headers:{
-    //         Authorization:`Basic ${basicToken}`,
-    //         "Content-Type":"application/json",
-    //     },
-    // }).then((res)=> res.json());
 
     //TODO: DB처리
-    // res.redirect(`/payments/complete?orderId=${orderId}`);
+    res.redirect(`/payments/complete?orderId=${orderId}`);
 }
