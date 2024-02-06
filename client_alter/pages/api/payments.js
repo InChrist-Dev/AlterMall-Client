f3cf5b9daad39bf14aedd51c6954e70f0755fc5c
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-06 13:22:24 +0900

    d

--- client_alter/pages/api/payments.js
+++ client_alter/pages/api/payments.js
@@ -11,26 +11,26 @@ export default async function handler(req, res) {
     // const basicToken = Buffer.from(`${secretKey}:`, `utf-8`).toString("base64");
 
    
-    try {
-        await fetch('https://udtown.site/customer/confirm/', {
-            method: 'patch',
-            body: JSON.stringify({
-                "order_id": orderId,
-                "payment_key": paymentKey,
-                "amount": amount,
+    // try {
+    //     await fetch('https://udtown.site/customer/confirm/', {
+    //         method: 'patch',
+    //         body: JSON.stringify({
+    //             "order_id": orderId,
+    //             "payment_key": paymentKey,
+    //             "amount": amount,
 
 
-            }),
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${accessToken}`,
+    //         }),
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Authorization: `Bearer ${accessToken}`,
                
-            },
-            credentials: 'include',
-        }).then((res) => console.log(res.json()));
-    } catch (error) {
-        console.error('데이터를 불러오는 중 오류가 발생했습니다:', error);
-    }
+    //         },
+    //         credentials: 'include',
+    //     }).then((res) => console.log(res.json()));
+    // } catch (error) {
+    //     console.error('데이터를 불러오는 중 오류가 발생했습니다:', error);
+    // }
 
     // await fetch(url,{
     //     method:'post',
