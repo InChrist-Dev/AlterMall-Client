d103a0f6b50de33e15e2ab75601c58105bf42e0a
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-07 13:28:54 +0900

    fix:액세스토큰 전송코드 수정

--- client_alter/app/user/[id]/pages/orders.js
+++ client_alter/app/user/[id]/pages/orders.js
@@ -3,7 +3,10 @@
 import React, { useEffect } from 'react';
 import styles from './pages.module.css';
 import { fetchData } from 'next-auth/client/_utils';
+import Cookies from 'js-cookie';
 
+// 쿠키에서 토큰을 가져오기
+const accessToken = Cookies.get('accessToken');
 const OrderHistory = () => {
   try{
     const fetchData = async()=>{
