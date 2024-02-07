345a46319a2839c8468432ce106cd858384fc1ff
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-07 12:07:59 +0900

    s

--- client_alter/app/payments/complete/page.js
+++ client_alter/app/payments/complete/page.js
@@ -15,7 +15,7 @@ export default async function Complete({searchParams}) {
   // const data = await response.json();
 
   // 데이터를 성공적으로 가져왔을 때 처리 로직을 추가합니다.
-  console.log(searchParams);
+  console.log(searchParams.orderId);
   // const { card } = data.data.rows[0];
 
   return (
