886a7da0f978d152dbadddc28466e78e24146dac
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-07 18:45:42 +0900

    fix:로그인 성공 페이지 수정

--- client_alter/app/login/page.js
+++ client_alter/app/login/page.js
@@ -8,16 +8,12 @@ export default async function handler(){
   try{
     const parsedHash = new URLSearchParams(window.location.search.substring(1));
     const accessToken = parsedHash.get("accessToken");
-    console.log(`${accessToken} query is`)
-    Cookies.set('accessToken', accessToken, { expires: 7 });  // 7일 동안 유지되도록 설정
-    const url = `https://udtown.site/auth/google`;
-    const successLogin=()=>{
-      alert('로그인 되었습니다');
-      window.location.href='https://altermall.shop';
-    }
 
-   
-    return (<button onClick={successLogin()}/>);
+    Cookies.set('accessToken', accessToken, { expires: 7 });  // 7일 동안 유지되도록 설정
+
+    alert('로그인 되었습니다');
+    window.location.href='https://altermall.shop';
+    
   }
   catch(error){
     console.error(error);
