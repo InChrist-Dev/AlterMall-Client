8259a36b00e8f8c5d03ebd4dc3bda33a56dc49c3
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-13 16:52:12 +0900

    feature:seller로그인구현

---
+++ client_alter/app/loginf/page.js
@@ -0,0 +1,61 @@
+'use client'
+import { useState } from 'react';
+import styles from './loginf.module.css';
+
+export default function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // 여기에서는 로그인 정보를 확인하고 처리하는 로직을 추가해야 합니다.
+    console.log("Username:", username);
+    console.log("Password:", password);
+    try {
+        const response = await fetch('https://udtown.site/auth/seller', {
+          method: 'POST',
+          body: JSON.stringify({
+            'id':username,
+            'pw': password,
+          }), 
+        });
+  
+        if (response.status == 200) {
+         
+    
+        } else if (response.status == 201) {
+          alert('저장되었습니다');
+          closeModal();
+        } else {
+          
+        }
+      } catch (error) {
+        // 에러 처리
+        console.error('Failed to send like request', error);
+      }
+  };
+  
+
+  return (
+    <div className={styles.container}>
+      <h1>로그인</h1>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <label className={styles.label} htmlFor="username">아이디</label>
+        <input className={styles.input}
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label className={styles.label} htmlFor="password">비밀번호</label>
+        <input className={styles.input}
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className={styles.button} type="submit">확인</button>
+      </form>
+    </div>
+  );
+}
