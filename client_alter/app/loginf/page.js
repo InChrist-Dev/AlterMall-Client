1584da0f9c66c90f06e9d913c89ff3dc662fdc0a
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-13 18:17:31 +0900

    fix:password타입 수정

--- client_alter/app/loginf/page.js
+++ client_alter/app/loginf/page.js
@@ -48,7 +48,7 @@ export default function LoginPage() {
         />
         <label className={styles.label} htmlFor="password">비밀번호</label>
         <input className={styles.input}
-          type="text"
+          type="password"
           id="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
