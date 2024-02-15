8273c06fc80cae8271c139f70fb51c577b0abb4d
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-15 17:18:21 +0900

    d

--- client_alter/app/loginPage/page.js
+++ client_alter/app/loginPage/page.js
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from 'react';
-import styles from './login_seller.module.css';
+import styles from './login_selller.module.css';
 import Cookies from 'js-cookie';
 
 export default function LoginPage() {
