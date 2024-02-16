1b5d457ebc2ed1dfa7c5da8974a11b530956fe25
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-16 10:51:30 +0900

    feature: 수락버튼 상태변화 수정

--- client_alter/app/admin_seller/page.js
+++ client_alter/app/admin_seller/page.js
@@ -1,6 +1,6 @@
 // ItemPage.jsx
 'use client'
-import React, { useEffect, useState,useCallback } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './admin_seller.module.css'; // Import the CSS module
 import { v4 as uuidv4 } from 'uuid';
@@ -11,9 +11,9 @@ import Cookies from 'js-cookie';
 const accessToken = Cookies.get('accessToken');
 
 const ItemPage = (props) => {
-  const [orders,setOrders] = useState([]);
-  const [items,setItems] = useState([]);
-  const [selectedOrder, setSelectedOrder] = useState(false); 
+  const [orders, setOrders] = useState([]);
+  const [items, setItems] = useState([]);
+  const [selectedOrder, setSelectedOrder] = useState(false);
   const myUuid = uuidv4();
   console.log(myUuid);
   const fetchData = async () => {
@@ -23,7 +23,7 @@ const ItemPage = (props) => {
           Authorization: `Bearer ${accessToken}`,
           'Content-Type': 'application/json',
         },
-        });
+      });
       const data = await response.json();
       console.log(data.data.rows);
       setOrders(data.data.rows);
@@ -38,11 +38,11 @@ const ItemPage = (props) => {
           Authorization: `Bearer ${accessToken}`,
           'Content-Type': 'application/json',
         },
-        });
+      });
       const data = await response.json();
       console.log(data.data.rows);
       setItems(data.data.rows);
-   
+
 
     } catch (error) {
       console.error('데이터를 불러오는 중 오류가 발생했습니다:', error);
@@ -50,19 +50,20 @@ const ItemPage = (props) => {
 
   };
   const handleQuantityChange = (index, newAmount) => {
-   
+
     if (newAmount >= 0) {
-   
+
       const updatedItems = [...items];
       updatedItems[index].stock = newAmount;
       setItems(updatedItems);
     } else {
-    
+
 
       const updatedItems = [...items];
       updatedItems[index].stock = 0;
       setItems(updatedItems);
-    }};
+    }
+  };
   // useEffect 안에서 fetchData 함수를 호출합니다.
   useEffect(() => {
     fetchData();
@@ -70,14 +71,14 @@ const ItemPage = (props) => {
 
   const Cancel = useCallback(
     (id) => {
-     
+
       fetch(`https://udtown.site/category/${id}`, {
         method: 'DELETE',
         headers: {
           Authorization: `Bearer ${accessToken}`,
           'Content-Type': 'application/json',
         },
-    
+
       })
         .then((response) => {
           if (response.status == 405) {
@@ -97,8 +98,8 @@ const ItemPage = (props) => {
   );
 
   const Update = useCallback(
-    (id,stock) => {
-     
+    (id, stock) => {
+
       fetch(`https://udtown.site/category/${id}`, {
         method: 'PATCH',
         headers: {
@@ -107,9 +108,9 @@ const ItemPage = (props) => {
         },
         body: JSON.stringify({
           'stock': stock,
-     
+
         }),
-    
+
       })
         .then((response) => {
           if (response.status == 405) {
@@ -128,15 +129,15 @@ const ItemPage = (props) => {
     [],
   );
   return (
-    <div style={{'marginBottom':'100px'}}>
+    <div style={{ 'marginBottom': '100px' }}>
       <h1 className={styles.title}>관리자 주문/조회</h1>
       <div className={styles.basketContainer}>
         <table className={styles.productTable}>
           <thead>
             <tr>
-           
+
               <th>이미지</th>
-             <th>상품명</th>
+              <th>상품명</th>
               <th>가격</th>
               {/* <th>취소</th> */}
               <th>재고</th>
@@ -145,9 +146,9 @@ const ItemPage = (props) => {
           <tbody>
             {items.map((items, index) => (
               <tr key={index} className={styles.productCard}>
-              
-                <td style={{display:'flex' , alignItems: 'center',}}>
-                  
+
+                <td style={{ display: 'flex', alignItems: 'center', }}>
+
                   <img
                     src={`https://udtown.site/${items.img}`}
                     alt={items.item_name}
@@ -168,7 +169,7 @@ const ItemPage = (props) => {
                 </td> */}
                 <td>
                   <div className={styles.quantityControl}>
-                  <button
+                    <button
                       onClick={() =>
                         handleQuantityChange(index, items.stock - 1)
                       }
@@ -177,13 +178,13 @@ const ItemPage = (props) => {
                     <span>{items.stock}</span>
                     <button
                       onClick={() =>
-                        handleQuantityChange(index,  items.stock + 1)
+                        handleQuantityChange(index, items.stock + 1)
                       }
                     >+
                     </button>
                     <button
                       onClick={() =>
-                        Update(items.item_id,items.stock)
+                        Update(items.item_id, items.stock)
                       }
                     >재고저장
                     </button>
@@ -192,16 +193,16 @@ const ItemPage = (props) => {
               </tr>
             ))}
           </tbody>
-          
+
         </table>
         <table className={styles.orderTable}>
-        <thead>
+          <thead>
             <tr>
-            <th>이미지</th>
+              <th>이미지</th>
               <th>주문 ID</th>
               <th>상품명</th>
               <th>가격</th>
-              
+
               <th>주문 일자</th>
               <th>주문자 정보</th>
               <th>배송 정보</th>
@@ -212,87 +213,90 @@ const ItemPage = (props) => {
           <tbody>
             {orders.map((order, index) => (
               <>
-              <tr key={index} className={styles.orderRow}>
-                   <img
+                <tr key={index} className={styles.orderRow}>
+                  <img
                     src={`https://udtown.site/${order.OrderDetails[0].img}`}
                     alt={order.OrderDetails[0].item_name}
                     className={styles.productImage}
                   />
-                <td>{order.order_id}</td>
-                <td>{order.OrderDetails[0].item_name}외 {order.OrderDetails[0].length}건</td>
-                <td>{order.amount}원</td>
-              
-                <td>{order.updatedAt}</td>
-                <td>
-                  <p>주문자명: {order.customer_name}</p>
-                  <p>연락처: {order.phone}</p>
-                  <p>우편번호: {order.post}</p>
-                  <p>주소: {order.addr} {order.addr_detail}</p>
-                </td>
-                <td>
-                  <p>배송 유형: {order.delivery_type}</p>
-                  <p>요청 사항: {order.requests}</p>
-                </td>
-                <td>{order.state}</td>
-                <td>
-                  <button
-                    className={styles.accessButton}
-                    
-                  >
-                    수락
-                  </button>
-                  <button
-                  className={styles.detailBtn}
-                  onClick={() => setSelectedOrder(!selectedOrder)}
-                >
-                  상세보기
-                </button>
-                </td>
-              {/* 선택된 주문에 대한 상세 정보를 나타내는 부분 */}
-     
-              </tr>
-              <tr>
-                <td colSpan="9">
-                {selectedOrder && (
-        <div >
+                  <td>{order.order_id}</td>
+                  <td>{order.OrderDetails[0].item_name}외 {order.OrderDetails[0].length}건</td>
+                  <td>{order.amount}원</td>
 
-          <table className={styles.detailTable}>
-            <thead>
-              <tr>
-              <th>이미지</th>
-                <th>상품명</th>
-                <th>가격</th>
-                <th>갯수</th>
-              </tr>
-            </thead>
-            <tbody>
-              {order.OrderDetails.map((detail, index) => (
-                <tr key={index}>
-                  <td>  <img
-                    src={`https://udtown.site/${detail.img} `}
-                    alt={detail.item_name} 
-                    className={styles.productImage}
-                  /></td>
-                  <td>{detail.item_name}</td>
-                  <td>{detail.price}원</td>
-                  <td>{detail.stock}</td>
+                  <td>{order.updatedAt}</td>
+                  <td>
+                    <p>주문자명: {order.customer_name}</p>
+                    <p>연락처: {order.phone}</p>
+                    <p>우편번호: {order.post}</p>
+                    <p>주소: {order.addr} {order.addr_detail}</p>
+                  </td>
+                  <td>
+                    <p>배송 유형: {order.delivery_type}</p>
+                    <p>요청 사항: {order.requests}</p>
+                  </td>
+                  <td>{order.state}</td>
+                  <td>
+                    {order.state === 'paid' ? (
+                      <button className={styles.accessButton}>
+                        수락
+                      </button>
+                    ) : order.state === 'accept' ? (
+                      <button className={styles.accessButton}>
+                        완료
+                      </button>
+                    ) : null}
+                    <button
+                      className={styles.detailBtn}
+                      onClick={() => setSelectedOrder(!selectedOrder)}
+                    >
+                      상세보기
+                    </button>
+                  </td>
+                  {/* 선택된 주문에 대한 상세 정보를 나타내는 부분 */}
+
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
-                </td>
-              </tr>
-         
+                <tr>
+                  <td colSpan="9">
+                    {selectedOrder && (
+                      <div >
+
+                        <table className={styles.detailTable}>
+                          <thead>
+                            <tr>
+                              <th>이미지</th>
+                              <th>상품명</th>
+                              <th>가격</th>
+                              <th>갯수</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {order.OrderDetails.map((detail, index) => (
+                              <tr key={index}>
+                                <td>  <img
+                                  src={`https://udtown.site/${detail.img} `}
+                                  alt={detail.item_name}
+                                  className={styles.productImage}
+                                /></td>
+                                <td>{detail.item_name}</td>
+                                <td>{detail.price}원</td>
+                                <td>{detail.stock}</td>
+                              </tr>
+                            ))}
+                          </tbody>
+                        </table>
+                      </div>
+                    )}
+                  </td>
+                </tr>
+
               </>
             ))}
           </tbody>
-          </table>
+        </table>
       </div>
-      
-      
-        
+
+
+
     </div>
   );
 };
