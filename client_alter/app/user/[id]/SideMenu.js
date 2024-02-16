8b1a8c981e40fc6044c7f32e95930ea0762a9967
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-16 12:28:21 +0900

    f

--- client_alter/app/user/[id]/SideMenu.js
+++ client_alter/app/user/[id]/SideMenu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './user.module.css';
-import Link from 'next/link';
+import { Link } from 'react-router-dom';
 
 import { signOut } from 'next-auth/react';
 import Cookies from 'js-cookie';
