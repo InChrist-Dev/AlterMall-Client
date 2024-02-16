c0ef3a188cd6069345d9490208c7c3f44f8f7aeb
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-16 11:13:54 +0900

    d

--- client_alter/app/component/navigation.js
+++ client_alter/app/component/navigation.js
@@ -5,13 +5,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 import { LoginBtn } from '../logins';
-
+import { UserBtn } from './userIcon';
+import { LogOutBtn } from '../logout';
 import Cookies from 'js-cookie';
 
 // 쿠키에서 토큰을 가져오기
 const accessToken = Cookies.get('accessToken');
-const NavigationBar = () => {
- 
+const NavigationBar = (session) => {
+  console.log(session)
   const [search, setSearch] = useState('');
   const [relatedKeywords, setRelatedKeywords] = useState([]);
   const [isKeywordMenu, setIsKeywordMenu] = useState(true);
