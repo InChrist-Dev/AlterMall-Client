e0457d510427a8fd2255d39f43d4fed948fe77ef
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-20 01:11:12 +0900

    d

--- client_alter/app/products/[id]/page.js
+++ client_alter/app/products/[id]/page.js
@@ -43,7 +43,7 @@ const ItemPage = (props) => {
         method: 'POST',
         headers: {
           Authorization: `Bearer ${accessToken}`,
-          'Content-Type': 'application/json',
+          'Content-Type': 'multipart/form-data',
         },
         credentials: 'include',
         body: JSON.stringify({
