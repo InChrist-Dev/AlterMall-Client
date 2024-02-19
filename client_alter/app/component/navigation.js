5455578ba2d645725b64bfbf171b1d80f5efdea2
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-20 00:30:17 +0900

    ㄴ

--- client_alter/app/component/navigation.js
+++ client_alter/app/component/navigation.js
@@ -149,7 +149,7 @@ const NavigationBar = (session) => {
               <li><a href="/master">장인소개</a></li>
               <li><a href="/ranking">랭킹</a></li>
               <li><a href="/">이벤트</a></li>
-              <li><a href="/">이달의 특가</a></li>
+              <li><a href="/">건강정보</a></li>
 
 
             </ul>
