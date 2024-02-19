e40adf191a48634ddb777f847828b83f0424e879
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-19 16:37:28 +0900

    e

--- client_alter/app/layout.js
+++ client_alter/app/layout.js
@@ -3,29 +3,23 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import NavigationBar from './component/navigation';
 import Footer from './component/footer';
-import { authOptions } from "@/pages/api/auth/[...nextauth].js"
-import { getServerSession } from "next-auth";
+
 import Cookies from 'js-cookie';
 
 // 쿠키에서 토큰을 가져오기
 const accessToken = Cookies.get('accessToken');
 const inter = Inter({ subsets: ['latin'] })
 
-export async function Session() {
-  let session = await getServerSession(authOptions);
-  console.log(session)
-  return session;
-}
+
 
 export default async function RootLayout({ children }) {
-  let session = await getServerSession(authOptions)
-  
+ 
 
   return (
     
     <html className="container" lang="ko">
       <body className={inter.className}> 
-      <NavigationBar session={session? session:''}/>
+      <NavigationBar/>
     
             
       {children}</body>
