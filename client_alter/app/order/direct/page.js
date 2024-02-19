2a659becf8b533074a24f6846ebe9cef1bfe1caf
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-19 21:02:41 +0900

    d

--- client_alter/app/order/direct/page.js
+++ client_alter/app/order/direct/page.js
@@ -7,9 +7,10 @@ import DeliveryInfoModal from '../Modal';
 import { loadTossPayments } from '@tosspayments/payment-sdk';
 import Cookies from 'js-cookie';
 import { v4 as uuidv4 } from 'uuid';
+
 // 쿠키에서 토큰을 가져오기
 const myUuid = uuidv4();
-  console.log(myUuid);
+console.log(myUuid);
 const accessToken = Cookies.get('accessToken');
 const Checkout = (props) => {
   const [showModal, setShowModal] = useState(false);
@@ -17,7 +18,7 @@ const Checkout = (props) => {
   const [delivery, setDelivery] = useState([]);
   // 간단한 상태 관리를 위해 useState 사용
   const [deliveryInfo, setDeliveryInfo] = useState('normal');
-  const [deliveryPay,setDeliveryPay] = useState(4000);
+  const [deliveryPay, setDeliveryPay] = useState(4000);
   const [selectedItems, setSelectedItems] = useState([]);
   const [quantity, setQuantity] = useState([]);
   const [items, setItems] = useState([]);
@@ -25,7 +26,8 @@ const Checkout = (props) => {
   const [amounts, setAmounts] = useState(1);
   const [requestOption, setRequestOption] = useState(''); // 선택한 요청사항
   const [customRequest, setCustomRequest] = useState(''); // 직접 입력한 요청사항
-  const [refresh,setRefresh] = useState(false);
+  const [refresh, setRefresh] = useState(false);
+
   // 라디오 버튼 선택 시 호출되는 함수
   const handleOptionChange = (e) => {
     setRequestOption(e.target.value);
@@ -39,6 +41,7 @@ const Checkout = (props) => {
   const handleCustomRequestChange = (e) => {
     setCustomRequest(e.target.value);
   };
+
   const handleClick = async () => {
     let amount = amounts;
     // 상품 목록을 표시하는 부분에서 첫 번째 상품의 이름을 추출합니다.
@@ -47,103 +50,70 @@ const Checkout = (props) => {
     // 첫 번째 상품 이외의 상품 개수를 계산합니다.
     const otherItemsCount = items.length > 1 ? items.length - 1 : 0;
 
-    const tosspayments = await loadTossPayments(
-      'live_ck_E92LAa5PVbPo4JbZKdGB87YmpXyJ'
-    );
-   
+    const tosspayments = await loadTossPayments('live_ck_E92LAa5PVbPo4JbZKdGB87YmpXyJ');
+
     amount += getSub();
-   
+
     if (delivery) {
       console.log(items);
 
-    
-      await fetch('https://udtown.site/customer/order',{
-        method:'post',
+      await fetch('https://udtown.site/customer/order', {
+        method: 'post',
         headers: {
           Authorization: `Bearer ${accessToken}`,
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          
           'seller_id': items.seller_id,
-          'order_id':myUuid,
+          'order_id': myUuid,
           'addr': delivery.addr,
           'addr_detail': delivery.addr_detail,
           'requests': requestOption,
           'amount': amount,
           'delivery_type': deliveryInfo,
           'phone': delivery.phone,
-          'customer_name':delivery.name,
+          'customer_name': delivery.name,
         }),
-          
-          
-        
-    
-    }).then(async (response) => {
-      const data =await  response.json();
-      console.log(data);
-      if (response.status == 405) {
-        alert('주문 실패하였습니다');
-      } else if (response.status == 201) {
-        await fetch('https://udtown.site/customer/orderdetail',{
-          method:'post',
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            items: orderItems,
-          }),
-            
-            
-          
-      
       }).then(async (response) => {
+        const data = await response.json();
+        console.log(data);
         if (response.status == 405) {
           alert('주문 실패하였습니다');
         } else if (response.status == 201) {
-        
-         
-          const data = await response.json();
-          console.log(data)
+          await fetch('https://udtown.site/customer/orderdetail', {
+            method: 'post',
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              items: orderItems,
+            }),
+          }).then(async (response) => {
+            if (response.status == 405) {
+              alert('주문 실패하였습니다');
+            } else if (response.status == 201) {
+              const data = await response.json();
+              console.log(data)
+            }
+          }).finally(
+
+          )
+          await tosspayments.requestPayment('카드', {
+            orderId: myUuid,
+            amount: amount,
+            orderName: `${firstItemName}외 ${otherItemsCount}건`,
+            successUrl: 'https://udtown.site/customer/confirm',
+            failUrl: window.location.origin,
+          });
         }
-    
-    
-      }).finally(
-    
-      )
-      await tosspayments.requestPayment('카드', {
-        orderId: myUuid,
-        amount: amount,
-        orderName: `${firstItemName}외 ${otherItemsCount}건`,
-        successUrl: 'https://udtown.site/customer/confirm',
-        failUrl: window.location.origin,
+      }).catch((error) => {
+        console.error('주문 처리 중 오류가 발생했습니다:', error);
       });
-
     } else {
-      alert('배송지를 먼저 등록해주세요')
+      alert('배송지를 먼저 등록해주세요');
     }
-  })
-
-
-
-
-
-    // }).then(async (response) => {
-    //   if (response.status == 405) {
-    //     alert('주문 실패하였습니다');
-    //   } else if (response.status == 201) {
-    //     alert('주문페이지로 넘어갑니다');
-    //     console.log(response);
-    //     const data = await response.json();
-    //     console.log(data)
-    //   }
-
-
-    // }).finally(
-
-    // )
-  }
+  };
 
   // Function to open the modal
   const openModal = () => {
@@ -153,31 +123,7 @@ const Checkout = (props) => {
   // Function to close the modal
   const closeModal = () => {
     setShowModal(false);
-   
   };
-  const toggleItemSelection = (index) => {
-    const newSelectedItems = [...selectedItems];
-    if (newSelectedItems.includes(index)) {
-      newSelectedItems.splice(newSelectedItems.indexOf(index), 1);
-    } else {
-      newSelectedItems.push(index);
-    }
-    setSelectedItems(newSelectedItems);
-  };
-  const calculateTotalPrice = () => {
-    return items.price * amounts;
-   
-    
-  };
-
-  // const toggleAllItemsSelection = () => {
-  //   if (selectedItems.length === items.length) {
-  //     setSelectedItems([]);
-  //   } else {
-  //     setSelectedItems([...Array(items.length).keys()]);
-  //   }
-  // };
-
 
   const getImageUrl = () => {
     // 이미지 주소는 사용자가 제공한 것을 사용합니다.
@@ -188,15 +134,17 @@ const Checkout = (props) => {
     }
     // 다른 배송 방법에 대한 이미지 주소를 추가할 수 있습니다.
   };
+
   const getPay = () => {
     // 이미지 주소는 사용자가 제공한 것을 사용합니다.
     if (deliveryInfo == 'normal') {
-      return 4000+calculateTotalPrice();
+      return 4000 + calculateTotalPrice();
     } else if (deliveryInfo == 'daily') {
-      return 4000+calculateTotalPrice();
+      return 4000 + calculateTotalPrice();
     }
     // 다른 배송 방법에 대한 이미지 주소를 추가할 수 있습니다.
   };
+
   const getSub = () => {
     // 이미지 주소는 사용자가 제공한 것을 사용합니다.
     if (deliveryInfo == 'normal') {
@@ -206,10 +154,11 @@ const Checkout = (props) => {
     }
     // 다른 배송 방법에 대한 이미지 주소를 추가할 수 있습니다.
   };
+
   const selDeliver = (id) => {
-    setDelivery(deliveryList[id])
-  }
-  // 전체 주문 가격 계산
+    setDelivery(deliveryList[id]);
+  };
+
   const handleQuantityChange = (index, newAmount) => {
     if (newAmount >= 0) {
       const newQuantity = { ...quantity };
@@ -228,31 +177,15 @@ const Checkout = (props) => {
       updatedItems[index].stock = 0;
       setItems(updatedItems);
     }
-
   };
+
   const fetchData = async () => {
     try {
-    
-       
-
-
       const response = await fetch(`https://udtown.site/category/${props.searchParams.itemId}`);
       const data = await response.json();
 
-      // 데이터를 성공적으로 가져왔을 때 처리 로직을 추가합니다.
-      // 데이터를 성공적으로 가져왔을 때 처리 로직을 추가합니다.
-      console.log(data);
-      setAmounts(props.searchParams.amount)
+      setAmounts(props.searchParams.amount);
       setItems(data);
-      
-    
-
-
-
-
-
-      // 데이터를 성공적으로 가져왔을 때 처리 로직을 추가합니다.
-
 
       const response2 = await fetch(`https://udtown.site/customer/deliver`, {
         headers: {
@@ -260,36 +193,22 @@ const Checkout = (props) => {
           'Content-Type': 'application/json',
         },
         credentials: 'include',
-
       });
       const data2 = await response2.json();
-      console.log(data2)
-     
+      console.log(data2);
+
       setDeliveryList(data2.data.rows);
-      setDelivery(data2.data.rows[0])
+      setDelivery(data2.data.rows[0]);
       setInfo(data.data.rows[0]);
-      
-
-      // const initialQuantity = data.data.rows[0].OrderDetails.map((item) => item.amount );
-
-      // setQuantity(initialQuantity);
-
-
-
-
-      // 데이터를 state로 업데이트하는 로직을 추가합니다.
-      // 예를 들어, setCategoryName(data.data.items.map(item => item.item_name));
-      // 필요한 모든 state를 업데이트해야 합니다.
     } catch (error) {
       console.error('데이터를 불러오는 중 오류가 발생했습니다:', error);
     }
   };
 
-  // useEffect 안에서 fetchData 함수를 호출합니다.
   useEffect(() => {
     fetchData();
   }, [refresh]);
-  console.log(deliveryList);
+
   return (
     <div className={styles.checkoutContainer}>
       <div style={{ display: showModal ? 'block' : 'none' }}>
@@ -302,9 +221,10 @@ const Checkout = (props) => {
           <div className={styles.deliveryInfo}>
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
               <h2 className={styles.categoryTitle}>배송지 정보</h2>
-              {/* 장인 버튼 */} <button className={styles.moreButton} onClick={openModal}>
+              <button className={styles.moreButton} onClick={openModal}>
                 배송지 변경 ▶
-              </button></div>
+              </button>
+            </div>
             <div className={styles.AddressBox}>
               {delivery ? (
                 <>
@@ -319,7 +239,6 @@ const Checkout = (props) => {
                   <div></div>
                 </>
               )}
-
             </div>
             <div style={{ border: '1px solid #ccc', marginTop: '50px', marginBottom: '20px' }}></div>
             <div className={styles.postBox}>
@@ -333,29 +252,26 @@ const Checkout = (props) => {
                 <option value="daily">당일 배송</option>
               </select>
               <img src={getImageUrl()} className={styles.postImage} alt="배송 이미지" />
-
             </div>
             <div className={styles.requestBox}>
-      <label>배송시 요청사항</label>
-      <div>
-       {/* 옵션 선택 */}
-       <select value={requestOption} onChange={handleOptionChange}>
-          <option value="">선택하세요</option>
-          <option value="노크x">노크x</option>
-          <option value="문앞에 두고 가주세요">문앞에 두고 가주세요</option>
-          <option value="직접입력">직접입력</option>
-        </select>
-        {/* 직접 입력 창 */}
-        {requestOption === '직접입력' && (
-          <input
-            className={styles.request}
-            value={customRequest}
-            placeholder="직접 입력해주세요"
-            onChange={handleCustomRequestChange}
-          />
-        )}
-      </div>
-    </div>
+              <label>배송시 요청사항</label>
+              <div>
+                <select value={requestOption} onChange={handleOptionChange}>
+                  <option value="">선택하세요</option>
+                  <option value="노크x">노크x</option>
+                  <option value="문앞에 두고 가주세요">문앞에 두고 가주세요</option>
+                  <option value="직접입력">직접입력</option>
+                </select>
+                {requestOption === '직접입력' && (
+                  <input
+                    className={styles.request}
+                    value={customRequest}
+                    placeholder="직접 입력해주세요"
+                    onChange={handleCustomRequestChange}
+                  />
+                )}
+              </div>
+            </div>
           </div>
 
           <div className={styles.orderItems}>
@@ -363,71 +279,33 @@ const Checkout = (props) => {
             <table className={styles.productTable}>
               <thead>
                 <tr>
-                  {/* <th>
-                    <input
-                      type="checkbox"
-                      className={styles.checkbox}
-                      checked={selectedItems.length === items.length}
-                      onChange={toggleAllItemsSelection}
-                    />
-
-                  </th> */}
                   <th>이미지</th>
                   <th>상품명</th>
                   <th>가격</th>
-              
                   <th>수량</th>
                 </tr>
               </thead>
               <tbody>
-                
-                  <tr className={styles.productCard}>
-                    {/* <td>
-                      <input
-                        type="checkbox"
-                        className={styles.checkbox}
-                        checked={selectedItems.includes(index)}
-                        onChange={() => toggleItemSelection(index)}
-                      />
-                    </td> */}
-                    <td style={{ display: 'flex', alignItems: 'center', }}>
-
+                {items.map((item, index) => (
+                  <tr key={index} className={styles.productCard}>
+                    <td style={{ display: 'flex', alignItems: 'center' }}>
                       <img
-                        src={`https://udtown.site/${items.img}`}
-                        alt={items.item_name}
+                        src={`https://udtown.site/${item.img}`}
+                        alt={item.item_name}
                         className={styles.productImage}
                       />
-
-                    
-
-
                     </td>
+                    <td>{item.item_name}</td>
                     <td>
-                    {items.item_name}
+                      <p>{item.price}원</p>
                     </td>
-                    <td>
-                      <p>{items.price}원</p>
-                    </td>
-                 
                     <td>
                       <div className={styles.quantityControl}>
-                        {/* <button
-                          onClick={() =>
-                            handleQuantityChange(index, items.stock - 1)
-                          }
-                        >-
-                        </button> */}
-                        <span>{items.stock}</span>
-                        {/* <button
-                          onClick={() =>
-                            handleQuantityChange(index, items.stock + 1)
-                          }
-                        >+
-                        </button> */}
+                        <span>{item.stock}</span>
                       </div>
                     </td>
                   </tr>
-              
+                ))}
               </tbody>
             </table>
           </div>
@@ -435,11 +313,9 @@ const Checkout = (props) => {
       </div>
 
       <div className={styles.stickySidebar}>
-        <h2 >주문 결제 금액</h2>
+        <h2>주문 결제 금액</h2>
         <div style={{ border: '1px solid #ddd', marginTop: '20px', marginBottom: '20px' }}></div>
-        <div> 상품금액:{calculateTotalPrice().toLocaleString()}원</div>
-        {/* <div>할인금액: 0원</div>
-        <div>상품권: 0원</div> */}
+        <div>상품금액: {calculateTotalPrice().toLocaleString()}원</div>
         <div>배송비: {getSub().toLocaleString()}원</div>
         <div>
           <strong>총 주문 가격:</strong> {getPay().toLocaleString()}원
