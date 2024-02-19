6cc02634f90de1b030312ee20d6082e7309e9c95
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-20 02:08:10 +0900

    d

--- client_alter/app/user/[id]/UserInfo.js
+++ client_alter/app/user/[id]/UserInfo.js
@@ -27,12 +27,12 @@ const UserInfo = (props) => {
       <div className={styles.lank}>프로건강러</div>
       <div><b>{props.name}</b>님, 안녕하세요!</div>
     </div>
-    <div className={styles.couInfo}>
+    {/* <div className={styles.couInfo}>
       주문배송
       <div className={styles.orderInfo}>
       <div className={styles.coupon}>0</div>
       </div>
-    </div>
+    </div> */}
     <div className={styles.couInfo}>
       쿠폰
       <div className={styles.orderInfo}>
