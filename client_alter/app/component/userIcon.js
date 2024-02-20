4f3eb1f604e00dbd592983d84d14a5920ade19bd
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-20 10:59:42 +0900

    d

--- client_alter/app/component/userIcon.js
+++
@@ -1,8 +0,0 @@
-'use client';
-import { faSearch, faShoppingCart, faUser, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { signOut } from 'next-auth/react'
-
-export function UserBtn(session){
-  return ( <FontAwesomeIcon   className="userIcon"icon={faUser} onClick={() => {  window.location.href = `/user/${session}`;}}/>)
-  } 
