0cdf6643e332ad595ef813e5c2eb83f0248c063e
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-02-22 00:00:55 +0900

    d

--- client_alter/app/admin_seller/page.js
+++ client_alter/app/admin_seller/page.js
@@ -368,7 +368,7 @@ return formattedDate;
                   <td>
                     <p>주문자명: {order.customer_name}</p>
                     <p>연락처: {order.phone}</p>
-                    <p>우편번호: {order.post}</p>
+                    {/* <p>우편번호: {order.post}</p> */}
                     <p>주소: <b>{order.addr} {order.addr_detail}</b></p>
                   </td>
                   <td>
