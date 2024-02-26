bc40bd6389aacd84b85a7c2eda037e637897ce33
ablecp <tasong12@kookmin.ac.kr>
2024-02-26 21:29:21 +0900

    feature: 점검페이지 구현

---
+++ client_alter/app/component/sos.js
@@ -0,0 +1,13 @@
+//점검페이지
+
+const Sos = () => {
+    return (
+    <div style={{"display": "flex", "flexDirection": "column", "justify-content":"center", "align-items": "center"}}>
+        <img style = {{"max-width": "400px"}} src="/alterMallIcon.jpg" alt="이미지1" />
+        <h2>얼터몰 서버는 현재 점검중입니다. 빠른 시일내에 복구하도록 하겠습니다.</h2>
+    </div>
+
+    );
+}
+
+export default Sos
