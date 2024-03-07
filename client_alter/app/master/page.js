a61df2e5330e1469d1d7b86951afcd4654764b88
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-03-07 16:41:25 +0900

    refactor:seller페이지 모바일 디자인

--- client_alter/app/master/page.js
+++ client_alter/app/master/page.js
@@ -16,7 +16,7 @@ const [favorites, setFavorites] = useState([false]); // Initialize with false fo
 
 const fetchData = async () => {
   try {
-    const response = await fetch(`https://altermall.site/User/seller`);
+    const response = await fetch(`http://localhost:8000/User/seller`);
     const data = await response.json();
 
     // 데이터를 성공적으로 가져왔을 때 처리 로직을 추가합니다.
