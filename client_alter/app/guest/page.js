d712303a4728c09dd08ba2c6e7622b61f3198d47
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-03-22 14:31:28 +0900

    d

--- client_alter/app/guest/page.js
+++ client_alter/app/guest/page.js
@@ -39,7 +39,7 @@ const Guest = (props) => {
           <h1 className={styles.title}>비회원 주문 조회</h1>
           {data ? (
             <div className={styles.orderInfo}>
-              <p>주문번호: {data.data.rows[0].order_id}</p>
+           
               <p>주문일시: {data.data.rows[0].createdAt}</p>
               <p>주문 상품 수: {data.data.totalItemCount}</p>
               <p>총 주문 금액: {data.data.rows[0].amount}원</p>
@@ -47,7 +47,7 @@ const Guest = (props) => {
               <div className={styles.productList}>
                 {data.data.rows[0].OrderDetails.map((detail, index) => (
                   <div key={index} className={styles.productItem}>
-                    <img src={detail.img} alt={detail.item_name} className={styles.productImage} />
+                    <img src={`https://altermall.site/${detail.img}`} alt={detail.item_name} className={styles.productImage} />
                     <p>{detail.item_name}</p>
                     <p>가격: {detail.price}원</p>
                     <p>수량: {detail.stock}</p>
