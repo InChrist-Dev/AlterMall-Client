7e2ea149318a582c56df0a6a1046bc0626300ebb
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-03-25 14:52:25 +0900

    s

--- client_alter/app/products/[id]/page.js
+++ client_alter/app/products/[id]/page.js
@@ -271,11 +271,11 @@ if (currentHour < 15) {
          // 비회원인 경우
          const orderInfo = {
           order_id: myUuid,
-          seller_id: guest[0].Item.seller_id,
+          seller_id: guest.seller_id,
           items: guest,
           // 여기에 필요한 다른 주문 정보를 추가합니다.
         };
-    localStorage.setItem('order', JSON.stringify(cartItems));
+    localStorage.setItem('order', JSON.stringify(orderInfo));
     alert('비회원 주문페이지로 이동합니다.');
     window.location.href=`/guestorder`;
   
