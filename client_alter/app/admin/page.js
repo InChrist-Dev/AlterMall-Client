e7c1e7ebd7293cb730c01c7f16b5cf6ec55df4b7
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-04-03 12:32:13 +0900

    d

--- client_alter/app/admin/page.js
+++ client_alter/app/admin/page.js
@@ -73,7 +73,61 @@ const ImageUploader = (props) => {
                 setUploadDisabled(false);
               });
   };
+  const handleImage = 
+  (event) => {
+    try{
+        if(item == ''){
+          setIsModalOpen(true);
+          setModal('제품의 이름을 입력해주세요')
+          event.preventDefault(); // 페이지 새로고침 방지
+        
 
+        }else{
+          confirm('업다운 컨텐츠를 저장하시겠습니까?');
+          
+          const formData = new FormData();
+          formData.append('item_id', item); // title 媛� 異붽��
+        
+        
+            formData.append(`desc`, files);
+         
+        
+    
+          fetch('https://altermall.site/category/image', {
+            method: 'POST',
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+      
+            },
+            credentials: 'include',
+            body: formData
+          })
+            .then(async(response) => {
+              const data = await response.json();
+              console.log(data)
+              console.log(response)
+              if(response.status == 404){
+                 
+                alert('컨텐츠 저장에 실패하였습니다');
+              }else if(response.status == 200){
+                alert('저장되었습니다');
+              }
+             
+             
+            })
+            .finally(() => {
+              setUploadDisabled(false);
+            });
+        }
+  
+               
+      
+    }catch(error){
+      
+      alert('컨텐츠 저장에 실패하였습니다')
+    }
+    
+  };
 
   const handleCancel = useCallback((index) => {
     setFiles((prevFiles) => {
@@ -173,65 +227,102 @@ const ImageUploader = (props) => {
     <div className="edit-bottom">
      
 
-      <div className={style.form}>
+     <div className={style.form}>
        
-          <div>
-            <label>
-              상품명
-              <input type="text" value={item_name} placeholder="상품명을 적어주세요" onChange={(event) => setItem_name(event.target.value)} />
-            </label>
-            <hr />
-            <label>
-              카테고리
-              <input type="text" value={category} placeholder="상품의 카테고리를 적어주세요" onChange={(event) => setCategory(event.target.value)} />
-            </label>
-            <hr />
-            <label>
-              가격
-              <input type="text" value={price} placeholder="제품의 가격을 적어주세요" onChange={(event) => setPrice(event.target.value)} />
-            </label>
-            <hr />
-            <label>
-              셀러 ID
-              <input type="text" value={seller_id} placeholder="판매자의 ID를 적어주세요" onChange={(event) => setSeller_id(event.target.value)} />
-            </label>
-            <hr />
-            <label>
-              재고
-              <input type="text" value={stock} placeholder="재고를 적어주세요" onChange={(event) => setStock(event.target.value)} />
-            </label>
-            <label>
-              배송비
-              <input type="text" value={delivery} placeholder="배송비를 적어주세요" onChange={(event) => setDelivery(event.target.value)} />
-            </label>
-            <hr />
-          </div>
-          <label style={{"fontSize":"15px"}}>이미지 업로드  </label>
-          <div {...getRootProps()} className={style.dropzone}>
-            <input {...getInputProps()} />
-            {files.length > 0 ? (
-              <div className={style.preview}>
-                {files.map((file, index) => (
-                  <div key={file.name} className={style.imageContainer}>
-                    <ImageWithAnimation src={URL.createObjectURL(file)} alt={file.name} className={style.image} />
-                    <button type="button" className={style.cancel} onClick={() => handleCancel(index)}>X</button>
-                  </div>
-                ))}
-              </div>
-            ) : (
-              <p onClick={() => { setUploadDisabled(false); }}>이곳에 이미지를 드래그하거나 클릭하여 업로드 해주세요.</p>
-            )}
-          </div>
-          <button className={style.button} onClick={()=>{handleSubmit()}}>저장</button>
-        <label>
-              아이템삭제
-              <input type="text" value={item} placeholder="아이템ID를 적어주세요" onChange={(event) => setItem(event.target.value)} />
-            </label>
-          <button className={style.modalButton} onClick={()=>{handleConfirm(item)}}>확인</button>
-  
+       <div>
+         <label>
+           상품명
+           <input type="text" value={item_name} placeholder="상품명을 적어주세요" onChange={(event) => setItem_name(event.target.value)} />
+         </label>
+         <hr />
+         <label>
+           카테고리
+           <input type="text" value={category} placeholder="상품의 카테고리를 적어주세요" onChange={(event) => setCategory(event.target.value)} />
+         </label>
+         <hr />
+         <label>
+           가격
+           <input type="text" value={price} placeholder="제품의 가격을 적어주세요" onChange={(event) => setPrice(event.target.value)} />
+         </label>
+         <hr />
+         <label>
+           셀러 ID
+           <input type="text" value={seller_id} placeholder="판매자의 ID를 적어주세요" onChange={(event) => setSeller_id(event.target.value)} />
+         </label>
+         <hr />
+         <label>
+           재고
+           <input type="text" value={stock} placeholder="재고를 적어주세요" onChange={(event) => setStock(event.target.value)} />
+         </label>
+         <label>
+           배송비
+           <input type="text" value={delivery} placeholder="배송비를 적어주세요" onChange={(event) => setDelivery(event.target.value)} />
+         </label>
+         <hr />
+       </div>
+       <label style={{"fontSize":"15px"}}>이미지 업로드  </label>
+       <div {...getRootProps()} className={style.dropzone}>
+         <input {...getInputProps()} />
+         {files.length > 0 ? (
+           <div className={style.preview}>
+             {files.map((file, index) => (
+               <div key={file.name} className={style.imageContainer}>
+                 <ImageWithAnimation src={URL.createObjectURL(file)} alt={file.name} className={style.image} />
+                 <button type="button" className={style.cancel} onClick={() => handleCancel(index)}>X</button>
+               </div>
+             ))}
+           </div>
+         ) : (
+           <p onClick={() => { setUploadDisabled(false); }}>이곳에 이미지를 드래그하거나 클릭하여 업로드 해주세요.</p>
+         )}
+       </div>
+       <button className={style.button} onClick={()=>{handleSubmit()}}>저장</button>
+     <label>
+           아이템삭제
+           <input type="text" value={item} placeholder="아이템ID를 적어주세요" onChange={(event) => setItem(event.target.value)} />
+         </label>
+       <button className={style.modalButton} onClick={()=>{handleConfirm(item)}}>확인</button>
 
-        
-      </div>
+
+     
+   </div>   <div className={style.form}>
+       
+       <div>
+   
+         <label>
+           아이템이미지등록
+           <input type="text" value={item} placeholder="판매자의 ID를 적어주세요" onChange={(event) => setSeller_id(event.target.value)} />
+         </label>
+         <hr />
+    
+   
+       </div>
+       <label style={{"fontSize":"15px"}}>이미지 업로드  </label>
+       <div {...getRootProps()} className={style.dropzone}>
+         <input {...getInputProps()} />
+         {files.length > 0 ? (
+           <div className={style.preview}>
+             {files.map((file, index) => (
+               <div key={file.name} className={style.imageContainer}>
+                 <ImageWithAnimation src={URL.createObjectURL(file)} alt={file.name} className={style.image} />
+                 <button type="button" className={style.cancel} onClick={() => handleCancel(index)}>X</button>
+               </div>
+             ))}
+           </div>
+         ) : (
+           <p onClick={() => { setUploadDisabled(false); }}>이곳에 이미지를 드래그하거나 클릭하여 업로드 해주세요.</p>
+         )}
+       </div>
+       <button className={style.button} onClick={()=>{handleImage()}}>저장</button>
+     <label>
+           아이템삭제
+           <input type="text" value={item} placeholder="아이템ID를 적어주세요" onChange={(event) => setItem(event.target.value)} />
+         </label>
+       <button className={style.modalButton} onClick={()=>{handleConfirm(item)}}>확인</button>
+
+
+     
+   </div>
     
     </div>
   );
