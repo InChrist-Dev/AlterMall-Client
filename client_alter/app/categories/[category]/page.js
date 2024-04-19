5cff2233a008ee5ba8f63824ce51c6e0ec70eff7
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-04-19 14:01:04 +0900

    d

--- client_alter/app/categories/[category]/page.js
+++ client_alter/app/categories/[category]/page.js
@@ -19,130 +19,8 @@ const ItemPage = (props) => {
   const [data, setData] = useState([])
   const category = props.params.category;
   console.log(category)
-  const testData = {
-    "exist": true,
-    "data": {
-      "count": 1,
-      "rows": [
-        {
-          "id": "rabe",
-          "addr": "경기도 의정부 범골로 107번 125-22 1",
-          "slogan": "건강한 쌀가루 빵, 라이스 베이커리",
-          "company_num": "123123-123123",
-          "bank": "농협 3021550941961 송채영",
-          "category": [
-            "dessert"
-          ],
-          "User": {
-            "name": "라베",
-            "profile": "upload/profiles/1707914495865.png"
-          },
-          "SellerDetail": {
-            "id": "rabe",
-            "img": "upload/profiles/1708586116510.jpeg",
-            "logo": "upload/profiles/1708586116510.png",
-            "title": "밀가루 없는 빵 라이스 베이커리",
-            "content": "Q. “사장님! 라베(RaBe)매장을 시작하게된 계기가 무엇인가요?”\nA. “둘째 딸이 피부가 갈라지는 건선 피부염이 있었어요.\n초등학교 여학생이던 딸이 디저트를 좋아했는데, 먹기만 하면 피부가 갈라져서 고민이었죠, 저도 당뇨가 있었는데 먹을 수 있는 음식이 제한되었고요\"\n\n“딸에게도, 저에게도 문제는 첨가물이었어요! 그래서 정제 당과 밀가루와 같은 첨가물이 안 들어간 쌀 빵을 만들었더니, 아이도 맛있어하고 건선 증상도 안정되더라고요!\"\n\n“그러던중, 딸 건강 문제로 함께 고민하던 남편이 건강한 디저트 음식점을 열어보라고 권유해서 시작하게 되었어요”",
-            "createdAt": "2024-02-18T14:46:35.000Z",
-            "updatedAt": "2024-02-22T07:15:16.000Z"
-          },
-          "Items": [
-            {
-              "item_id": "64063f97-606d-4fb7-92c2-689e4b8ce29f",
-              "item_name": "제로 홍국 쌀 식빵",
-              "price": 5400,
-              "stock": 5,
-              "max_stock": 5,
-              "isSelling": true,
-              "img": "upload/itemImgs/1708490645189.jpeg",
-              "category": "dessert",
-              "seller_id": "rabe",
-              "createdAt": "2024-02-18T06:21:10.000Z",
-              "updatedAt": "2024-03-26T05:07:33.000Z"
-            },
-            {
-              "item_id": "4bd5df3e-1f32-4aa4-ba11-6280b4b852e6",
-              "item_name": "제로 쑥 쌀 식빵",
-              "price": 5400,
-              "stock": 5,
-              "max_stock": 5,
-              "isSelling": true,
-              "img": "upload/itemImgs/1708490632801.jpeg",
-              "category": "dessert",
-              "seller_id": "rabe",
-              "createdAt": "2024-02-18T06:20:43.000Z",
-              "updatedAt": "2024-03-27T01:51:07.000Z"
-            },
-            {
-              "item_id": "36708253-4cc6-45c0-8acd-3ae5ac7edb92",
-              "item_name": "제로 백미 쌀 식빵",
-              "price": 5000,
-              "stock": 5,
-              "max_stock": 5,
-              "isSelling": true,
-              "img": "upload/itemImgs/1708490607301.jpeg",
-              "category": "dessert",
-              "seller_id": "rabe",
-              "createdAt": "2024-02-18T06:19:50.000Z",
-              "updatedAt": "2024-03-27T04:13:52.000Z"
-            },
-            {
-              "item_id": "1c8c25e8-4ceb-4b69-a0f9-bc121bbf72a5",
-              "item_name": "우유 쌀 식빵",
-              "price": 5400,
-              "stock": 5,
-              "max_stock": 5,
-              "isSelling": true,
-              "img": "upload/itemImgs/1708490585091.jpeg",
-              "category": "dessert",
-              "seller_id": "rabe",
-              "createdAt": "2024-02-16T07:33:52.000Z",
-              "updatedAt": "2024-03-27T01:51:02.000Z"
-            },
-            {
-              "item_id": "45d17570-6880-4a71-8f24-c9c447985cc3",
-              "item_name": "단호박 크림치즈 쌀 식빵",
-              "price": 6700,
-              "stock": 5,
-              "max_stock": 5,
-              "isSelling": true,
-              "img": "upload/itemImgs/1708490619889.jpeg",
-              "category": "dessert",
-              "seller_id": "rabe",
-              "createdAt": "2024-02-14T03:49:15.000Z",
-              "updatedAt": "2024-03-27T01:50:59.000Z"
-            },
-            {
-              "item_id": "8264c3b7-fa14-43d4-98d9-80d4070ed016",
-              "item_name": "오징어 먹물 치즈  쌀 식빵",
-              "price": 7000,
-              "stock": 5,
-              "max_stock": 5,
-              "isSelling": true,
-              "img": "upload/itemImgs/1708490671675.jpeg",
-              "category": "dessert",
-              "seller_id": "rabe",
-              "createdAt": "2024-02-14T03:48:59.000Z",
-              "updatedAt": "2024-03-26T05:07:33.000Z"
-            },
-            {
-              "item_id": "7db005f0-7ccc-4cd0-a5c8-3f67b2d20b86",
-              "item_name": "바질 올리브 치즈 쌀  식빵",
-              "price": 6700,
-              "stock": 5,
-              "max_stock": 5,
-              "isSelling": true,
-              "img": "upload/itemImgs/1708490658555.jpeg",
-              "category": "dessert",
-              "seller_id": "rabe",
-              "createdAt": "2024-02-14T03:48:35.000Z",
-              "updatedAt": "2024-03-27T01:50:52.000Z"
-            }
-          ]
-        }
-      ]
-    }
-  }
+ 
+  
   console.log(testData.data.rows[0]);
 
   const fetchData = async () => {
@@ -150,14 +28,9 @@ const ItemPage = (props) => {
       const response = await fetch(`https://altermall.site/category/sellers/${category}`);
       const data = await response.json();
       console.log(data)
-      // // 데이터를 성공적으로 가져왔을 때 처리 로직을 추가합니다.
-      // setCategoryList(data.data.items);
-      // setPage(data.data.totalPages);
-      // console.log(data.data)
+
       setData(data.data.rows)
-      // 데이터를 state로 업데이트하는 로직을 추가합니다.
-      // 예를 들어, setCategoryName(data.data.items.map(item => item.item_name));
-      // 필요한 모든 state를 업데이트해야 합니다.
+
     } catch (error) {
       console.error('데이터를 불러오는 중 오류가 발생했습니다:', error);
     }
