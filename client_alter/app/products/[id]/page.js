ab922ad69b77c9d32559f54c176b2cf05b7dd832
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-05-14 11:51:08 +0900

    d

--- client_alter/app/products/[id]/page.js
+++ client_alter/app/products/[id]/page.js
@@ -333,7 +333,7 @@ if (currentHour < 15) {
         <div className={styles.productInfo}>
           <div className={styles.productTitle}>
           <h1 >{name}</h1>
-          {/* {like?<FontAwesomeIcon onClick={()=>{likeBtn()}} style={{"color":"#f0571b"}} icon={solidHeart} size='2x'/>:<FontAwesomeIcon onClick={()=>{likeBtn()}} style={{"color":"#f0571b"}} icon={regularHeart} size='2x' />} */}
+          {like?<FontAwesomeIcon onClick={()=>{likeBtn()}} style={{"color":"#f0571b"}} icon={solidHeart} size='2x'/>:<FontAwesomeIcon onClick={()=>{likeBtn()}} style={{"color":"#f0571b"}} icon={regularHeart} size='2x' />} 
           </div>
          
         
