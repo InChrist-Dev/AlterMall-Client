7e631b7abfd20c50642973afa4cc3c7524ae8115
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-05-14 15:11:10 +0900

    d

--- client_alter/app/user/[id]/SideMenu.js
+++ client_alter/app/user/[id]/SideMenu.js
@@ -29,16 +29,16 @@ const SideMenu = () => {
         <li><Link to="/user/points" className={styles.menuItem}>포인트</Link></li>
       </ul> */}
   
-      {/* <div className={styles.menuCategory}>활동관리</div>
+      <div className={styles.menuCategory}>활동관리</div>
       <ul>
-        <li><Link to="/user/recent-products" className={styles.menuItem}>최근 본 상품</Link></li>
+        {/* <li><Link to="/user/recent-products" className={styles.menuItem}>최근 본 상품</Link></li> */}
         <li><Link to="/user/favorite-products" className={styles.menuItem}>찜한 상품</Link></li>
-        <li><Link to="/user/favorite-brands" className={styles.menuItem}>관심 브랜드</Link></li>
+        {/* <li><Link to="/user/favorite-brands" className={styles.menuItem}>관심 브랜드</Link></li>
         <li><Link to="/user/inquiries" className={styles.menuItem}>1:1문의</Link></li>
         <li><Link to="/user/product-reviews" className={styles.menuItem}>상품 후기</Link></li>
-        <li><Link to="/user/product-questions" className={styles.menuItem}>상품 문의내역</Link></li>
+        <li><Link to="/user/product-questions" className={styles.menuItem}>상품 문의내역</Link></li> */}
       </ul>
-   */}
+  
       <div className={styles.menuCategory}>회원정보관리</div>
       <ul>
         {/* <li><Link to="/user/shipping-address" className={styles.menuItem}>배송지 관리</Link></li>
@@ -50,6 +50,7 @@ const SideMenu = () => {
     
      <ul>
        <li><Link to="/user/orders" className={styles.menuItem}>주문내역</Link></li>
+       <li><Link to="/user/favorite-products" className={styles.menuItem}>찜한 상품</Link></li>
        {/* <li><Link to="/user/cancel" className={styles.menuItem}>취소/반품내역</Link></li>
        <li><Link to="/user/subscriptions" className={styles.menuItem}>정기배송 관리</Link></li>
        <li><Link to="/user/restock-alerts" className={styles.menuItem}>재입고 알림상품</Link></li> */}
@@ -64,7 +65,7 @@ const SideMenu = () => {
      {/* <div className={styles.menuCategory}>활동관리</div>
      <ul>
        <li><Link to="/user/recent-products" className={styles.menuItem}>최근 본 상품</Link></li>
-       <li><Link to="/user/favorite-products" className={styles.menuItem}>찜한 상품</Link></li>
+
        <li><Link to="/user/favorite-brands" className={styles.menuItem}>관심 브랜드</Link></li>
        <li><Link to="/user/inquiries" className={styles.menuItem}>1:1문의</Link></li>
        <li><Link to="/user/product-reviews" className={styles.menuItem}>상품 후기</Link></li>
