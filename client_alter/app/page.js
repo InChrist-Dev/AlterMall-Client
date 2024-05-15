1c57144d339e8f30b4bd9b6126df8383c5666229
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-05-15 19:16:47 +0900

    d

--- client_alter/app/page.js
+++ client_alter/app/page.js
@@ -1,12 +1,18 @@
 'use client'
 import React from 'react';
-
+import { useEffect } from 'react';
 import ImageSlider from './component/ImageSlider';
 import CategoryButtons from './component/category';
 
-
+import Cookies from 'js-cookie';
 const HomePage = () => {
-
+  useEffect(() => {
+    const position= Cookies.get('position');
+    if(position=='guest'){
+      Cookies.remove('accessToken');
+      Cookies.remove('position');
+    }
+  }, []);
 
   return (
     <div >
