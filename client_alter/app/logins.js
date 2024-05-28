6b886ef75782409ebe10087234aeac1579f49b99
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-05-28 12:00:20 +0900

    f

--- client_alter/app/logins.js
+++ client_alter/app/logins.js
@@ -1,7 +1,7 @@
 'use client';
-import { faSearch, faShoppingCart, faUser, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
 
 
  
