e1809132fa95ac60a0fc96ceb06dd350493f0d9a
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-06-01 10:38:42 +0900

    s

--- client_alter/app/component/ImageSlider.js
+++ client_alter/app/component/ImageSlider.js
@@ -1,45 +1,56 @@
 // components/ImageSlider.js
 'use client'
 import React, { useState, useEffect } from 'react';
-import Slider from 'react-slick';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import styles from '.././page.module.css';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 const ImageSlider = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    arrows: false,
-    appendDots: dots => (
-      <ul style={{ margin: "0px" }}> {dots} </ul>
-    )
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? 3 : prevIndex - 1));
   };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 3 ? 0 : prevIndex + 1));
+  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex === 3 ? 0 : prevIndex + 1));
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className={styles.slider}>
-      <Slider  className={styles.imageSlider}
+      <div
+        className={styles.imageSlider}
         style={{
           transform: `translateX(-${currentIndex * 100}%)`,
-        }} {...settings}>
-        <div>
-          <img src="/dapumda.png" alt="이미지3" className={styles.sliderImage} />
-        </div>
-        <div>
-          <img src="/003.png" alt="이미지3" className={styles.sliderImage} />
-        </div>
-        <div>
-          <img src="/001.png" alt="이미지1" className={styles.sliderImage} />
-        </div>
-        <div>
-          <img src="/002.png" alt="이미지2" className={styles.sliderImage} />
-        </div>
-      </Slider>
+        }}
+      >
+        {/* Images */}
+        <img src="/dapumda.png" alt="이미지3" className={styles.sliderImage} />
+        <img src="/003.png" alt="이미지3" className={styles.sliderImage} />
+        <img src="/001.png" alt="이미지1" className={styles.sliderImage} />
+        <img src="/002.png" alt="이미지2" className={styles.sliderImage} />
+        
+       
+      </div>
+
+      {/* Arrow Buttons */}
+      <FontAwesomeIcon
+        icon={faChevronLeft}
+        className={styles.leftArrow}
+        onClick={handlePrev}
+      />
+      <FontAwesomeIcon
+        icon={faChevronRight}
+        className={styles.rightArrow}
+        onClick={handleNext}
+      />
     </div>
   );
 };
