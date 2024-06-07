f195df4bbb420dcf627f10e3d83155d90a76566c
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-06-07 10:11:35 +0900

    d

--- client_alter/app/component/recommend.js
+++ client_alter/app/component/recommend.js
@@ -23,8 +23,8 @@ export default function Recommend(){
       const res_drink = await fetch(`https://altermall.site/category?sortby=highest&category=drink`);
       
       const free_data = await res_free.json();
-      const dessert_data = await res_dessert.json();
-      const drink_data = await res_drink.json();
+      const dessert_data = await res_dessert.data.items.json();
+      const drink_data = await res_drink.data.items.json();
     
       console.log(dessert_data)
       console.log(drink_data)
