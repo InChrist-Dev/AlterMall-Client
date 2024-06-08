6a1841f250dab66c92ff739812a1f90d345d5ecc
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-06-08 11:18:40 +0900

    d

--- client_alter/app/products/[id]/page.js
+++ client_alter/app/products/[id]/page.js
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState, useEffect, useCallback } from 'react';
 import styles from './products.module.css'; // Ensure the correct path to your CSS module
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
