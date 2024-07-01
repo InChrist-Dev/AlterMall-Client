00f7ad7462b780e5a8aafadccbce9b0f57836214
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-07-01 20:46:24 +0900

    s

--- client_alter/app/component/ImageSlider.js
+++ client_alter/app/component/ImageSlider.js
@@ -7,19 +7,30 @@ import styles from '.././page.module.css';
 
 const ImageSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const images = [
+    { src: "/zero.png", alt: "이미지0", class: `${styles.sliderImage} ${styles.smallOnly}` },
+    { src: "/popup.png", alt: "이미지1", class: `${styles.sliderImage} ${styles.smallOnly}` },
+    { src: "/003.png", alt: "이미지2", class: `${styles.sliderImage} ${styles.largeOnly}` },
+    { src: "/001.png", alt: "이미지3", class: `${styles.sliderImage} ${styles.largeOnly}` },
+    { src: "/002.png", alt: "이미지4", class: `${styles.sliderImage} ${styles.largeOnly}` },
+  ];
 
   const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? 3 : prevIndex ));
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
   };
 
   const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 3 ? 0 : prevIndex ));
+    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
   };
+
+  const handleDotClick = (index) => {
+    setCurrentIndex(index);
+  };
+
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex === 3 ? 0 : prevIndex));
+      handleNext();
     }, 5000);
-
     return () => clearInterval(interval);
   }, []);
 
@@ -31,16 +42,25 @@ const ImageSlider = () => {
           transform: `translateX(-${currentIndex * 100}%)`,
         }}
       >
-        {/* Images */}
-        <img src="/zero.png" alt="이미지3" className={`${styles.sliderImage} ${styles.smallOnly}`} />
-        <img src="/popup.png" alt="이미지3" className={`${styles.sliderImage} ${styles.smallOnly}`} />
-        <img src="/003.png" alt="이미지3" className={`${styles.sliderImage} ${styles.largeOnly}`} />
-        <img src="/001.png" alt="이미지1" className={`${styles.sliderImage} ${styles.largeOnly}`} />
-        <img src="/002.png" alt="이미지2" className={`${styles.sliderImage} ${styles.largeOnly}`}/>
-        
-       
+        {images.map((image, index) => (
+          <img key={index} src={image.src} alt={image.alt} className={image.class} />
+        ))}
       </div>
-
+      <button className={`${styles.sliderButton} ${styles.prev}`} onClick={handlePrev}>
+        <FontAwesomeIcon icon={faChevronLeft} />
+      </button>
+      <button className={`${styles.sliderButton} ${styles.next}`} onClick={handleNext}>
+        <FontAwesomeIcon icon={faChevronRight} />
+      </button>
+      <div className={styles.dots}>
+        {images.map((_, index) => (
+          <span
+            key={index}
+            className={`${styles.dot} ${index === currentIndex ? styles.activeDot : ''}`}
+            onClick={() => handleDotClick(index)}
+          ></span>
+        ))}
+      </div>
     </div>
   );
 };
