2522b4a12c653572e1a4472b7f15ec8b7ad4f2c5
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-07-03 18:16:36 +0900

    a

--- client_alter/app/component/ImageSlider.js
+++ client_alter/app/component/ImageSlider.js
@@ -1,12 +1,14 @@
 // components/ImageSlider.js
 'use client'
-import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from '.././page.module.css';
 
 const ImageSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [touchStart, setTouchStart] = useState(0);
+  const [touchEnd, setTouchEnd] = useState(0);
+  const sliderRef = useRef(null);
+
   const images = [
     { src: "/zero.png", alt: "이미지0", class: `${styles.sliderImage} ${styles.smallOnly}` },
     { src: "/popup.png", alt: "이미지1", class: `${styles.sliderImage} ${styles.smallOnly}` },
@@ -15,12 +17,34 @@ const ImageSlider = () => {
     { src: "/002.png", alt: "이미지4", class: `${styles.sliderImage} ${styles.largeOnly}` },
   ];
 
+  const handleSwipe = () => {
+    if (touchStart - touchEnd > 75) {
+      handleNext();
+    }
+
+    if (touchStart - touchEnd < -75) {
+      handlePrev();
+    }
+  };
+
+  const handleTouchStart = (e) => {
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+
+  const handleTouchMove = (e) => {
+    setTouchEnd(e.targetTouches[0].clientX);
+  };
+
+  const handleTouchEnd = () => {
+    handleSwipe();
+  };
+
   const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? 2 - 1 : prevIndex - 1));
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
   };
 
   const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 2 - 1 ? 0 : prevIndex + 1));
+    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
   };
 
   const handleDotClick = (index) => {
@@ -30,28 +54,25 @@ const ImageSlider = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       handleNext();
-    }, 7000);
+    }, 5000);
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className={styles.slider}>
+    <div className={styles.slider} ref={sliderRef}>
       <div
         className={styles.imageSlider}
         style={{
           transform: `translateX(-${currentIndex * 100}%)`,
         }}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
       >
         {images.map((image, index) => (
           <img key={index} src={image.src} alt={image.alt} className={image.class} />
         ))}
       </div>
-      <button className={`${styles.sliderButton} ${styles.prev}`} onClick={handlePrev}>
-        <FontAwesomeIcon icon={faChevronLeft} />
-      </button>
-      <button className={`${styles.sliderButton} ${styles.next}`} onClick={handleNext}>
-        <FontAwesomeIcon icon={faChevronRight} />
-      </button>
       <div className={styles.dots}>
         {images.map((_, index) => (
           <span
