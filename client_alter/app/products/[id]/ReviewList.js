44e62cc4b61a709e6ac027894e2b16abd712e2f8
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-07-16 21:39:46 +0900

    a

--- client_alter/app/products/[id]/ReviewList.js
+++ client_alter/app/products/[id]/ReviewList.js
@@ -1,9 +1,11 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './products.module.css';
 import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
 
 const ReviewList = ({ reviews, openModal }) => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   const maskUserId = (userId) => {
     const maskedLength = Math.ceil(userId.length / 2);
     const masked = '*'.repeat(maskedLength);
@@ -28,16 +30,19 @@ const ReviewList = ({ reviews, openModal }) => {
     return stars;
   };
 
+  const handleImageClick = (imageSrc) => {
+    setSelectedImage(imageSrc);
+  };
+
   return (
     <div className={styles.reviewList}>
       {reviews.map((review) => (
         <div key={review.id} className={styles.reviewItem} onClick={() => openModal(review)}>
           <div className={styles.reviewHeader}>
             <div className={styles.reviewName}>
-            <span className={styles.reviewAuthor}>{maskUserId(review.User.name)}</span>
-            <div className={styles.reviewRating}>{renderStars(review.rate)}</div>
+              <span className={styles.reviewAuthor}>{maskUserId(review.User.name)}</span>
+              <div className={styles.reviewRating}>{renderStars(review.rate)}</div>
             </div>
-  
             <span className={styles.reviewDate}>{new Date(review.createdAt).toLocaleDateString()}</span>
           </div>
           <div className={styles.reviewContent}>
@@ -45,13 +50,31 @@ const ReviewList = ({ reviews, openModal }) => {
               <img 
                 src={`https://altermall.site/${review.img}`} 
                 alt="Review" 
-                className={styles.reviewThumbnail} 
+                className={styles.reviewThumbnail}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleImageClick(`https://altermall.site/${review.img}`);
+                }}
               />
             )}
             <p className={styles.reviewText}>{review.content}</p>
           </div>
         </div>
       ))}
+      {selectedImage && (
+        <ImageModal imageSrc={selectedImage} onClose={() => setSelectedImage(null)} />
+      )}
+    </div>
+  );
+};
+
+const ImageModal = ({ imageSrc, onClose }) => {
+  return (
+    <div className={styles.imageModal} onClick={onClose}>
+      <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+        <img src={imageSrc} alt="Enlarged review" className={styles.enlargedImage} />
+        <button className={styles.closeButton} onClick={onClose}>Close</button>
+      </div>
     </div>
   );
 };
