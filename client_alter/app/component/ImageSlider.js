a4d6602e62436a1968d7548173bf7fd9e289f15b
dlwjdgns0308 <dlwjdgns0308@naver.com>
2024-07-23 12:31:13 +0900

    a

--- client_alter/app/component/ImageSlider.js
+++ client_alter/app/component/ImageSlider.js
@@ -89,11 +89,9 @@ const ImageSlider = () => {
         onTouchMove={handleTouchMove}
         onTouchEnd={handleTouchEnd}
       >
-    {images.map((image, index) => (
-  <a key={index} href={image.link} className={styles.sliderLink}>
-    <img src={image.src} alt={image.alt} className={styles.sliderImage} />
-  </a>
-))}
+        {images.map((image, index) => (
+          <img key={index} src={image.src} alt={image.alt} className={styles.sliderImage} />
+        ))}
       </div>
       <div className={styles.dots}>
         {images.map((_, index) => (
